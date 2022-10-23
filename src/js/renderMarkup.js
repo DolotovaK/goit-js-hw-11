762c7d87d5db3b56f3fcb02eb8603262a3f29a62
Kateryna Dolotova <104764377+DolotovaK@users.noreply.github.com>
2022-10-23 21:03:45 +0300

    update renderMarkup

--- src/js/renderMarkup.js
+++ src/js/renderMarkup.js
@@ -2,25 +2,35 @@ const galleryContainer = document.querySelector('.gallery');
 
 export function renderMarkup(arr) {
   return arr
-    .map(photo => {
-      return `<div class="photo-card">
-  <a href="${photo.largeImageURL}"><img src="${photo.webformatURL}" alt="${photo.tags}" loading="lazy" width = "300" height ="200"/></a>
+    .map(
+      ({
+        largeImageURL,
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+  <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" width = "300" height ="200"/></a>
   <div class="info">
     <p class="info-item">
-      <b>Likes:</b> ${photo.likes}
+      <b>Likes:</b> ${likes}
     </p>
     <p class="info-item">
-      <b>Views:</b> ${photo.views}
+      <b>Views:</b> ${views}
     </p>
     <p class="info-item">
-      <b>Comments:</b> ${photo.comments}
+      <b>Comments:</b> ${comments}
     </p>
     <p class="info-item">
-      <b>Downloads:</b> ${photo.comments}
+      <b>Downloads:</b> ${downloads}
     </p>
   </div>
 </div>`;
-    })
+      }
+    )
     .join('');
 }
 
